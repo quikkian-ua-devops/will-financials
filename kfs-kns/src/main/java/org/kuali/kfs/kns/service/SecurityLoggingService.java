d0ebe5b8c624925c1f3ce63feafabff79bad300b
Kevin Kronenbitter <kevin@kuali.co>
2017-04-24 16:08:17 -0600

    update license header

--- kfs-kns/src/main/java/org/kuali/kfs/kns/service/SecurityLoggingService.java
+++ kfs-kns/src/main/java/org/kuali/kfs/kns/service/SecurityLoggingService.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The Kuali Financial System, a comprehensive financial management system for higher education.
+ *
+ * Copyright 2005-2017 Kuali, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.kuali.kfs.kns.service;
 
@@ -13,7 +26,6 @@ import org.kuali.rice.krad.bo.BusinessObject;
 /**
  * A service that logs access to restricted information
  * 
- * @author wliang
  */
 public interface SecurityLoggingService {
 
