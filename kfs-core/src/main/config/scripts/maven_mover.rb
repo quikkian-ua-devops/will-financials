c4d1b2a620d29dfdd62ce5cf5de7298da9f29bd6
kkronenb <kevin@kuali.co>
2017-05-15 09:41:07 -0600

    FINI-3891 update license headers with plugin

--- kfs-core/src/main/config/scripts/maven_mover.rb
+++ kfs-core/src/main/config/scripts/maven_mover.rb
@@ -1,11 +1,12 @@
+#
 # The Kuali Financial System, a comprehensive financial management system for higher education.
 #
 # Copyright 2005-2017 Kuali, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,6 +15,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 # This helpful script moves files from the pre-KFS-6.0 directory structures
 # to the Maven standard layout.
